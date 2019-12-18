d1d1e2e338930e4895bc414d8f14b1c88402c389
matt-stam <mjstam@ilstu.edu>
2019-12-18 11:18:26 -0600

    add taking args

--- cmd/add.go
+++ cmd/add.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+Copyright © 2019 Matt Stam mattstam@live.com
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,16 +24,15 @@ import (
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Add a new todo",
+	Long: `Add will create a new todo item to list`,
+	Run: addRun,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
-	},
+func addRun(cmd *cobra.Command, args [] string) {
+	for _, x := range args {
+		fmt.Println(x);
+	}
 }
 
 func init() {
